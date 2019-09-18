d6b84bf1339a1607a1fcf7eb70691be00943ed18
whalemare <nanostet@gmail.com>
2019-09-18 13:11:39 +0700

    Add pass native value from android

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -1,17 +1,16 @@
 package com.example;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.PackageList;
-import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
-import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 
 import java.util.List;
+
+import ru.whalemare.rn.library.LibraryPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -27,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
+      packages.add(new LibraryPackage());
       return packages;
     }
 
